f7f7891fe0104e0f3c4ef70ef18e4fa9ae866edc
Adnan Travljanin <adnantravljanin@Adnans-MacBook-Pro.local>
2023-10-14 18:12:48 +0200

    instagram slider section

---
+++ assets/instagram-slider.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper(".instagram-slider", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  });
+  
