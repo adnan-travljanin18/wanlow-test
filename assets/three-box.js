8c6184ad2bc44d2972c6446f5b9f67b32964c7f6
Adnan Travljanin <adnantravljanin@Adnans-MacBook-Pro.local>
2023-10-15 20:50:59 +0200

    fixing up the grid section

---
+++ assets/three-box.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    grabCursor: true,
+    simulateTouch: true,
+    touchRatio: 0.5,
+  });
