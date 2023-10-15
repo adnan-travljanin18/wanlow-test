8631c4878a94b85b66c5b8fe61283baa5cc28c53
Adnan Travljanin <adnantravljanin@Adnans-MacBook-Pro.local>
2023-10-15 21:47:37 +0200

    fixing three box section

--- assets/three-box.js
+++ assets/three-box.js
@@ -1,7 +1,28 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: '3',
-    spaceBetween: 30,
-    grabCursor: true,
-    simulateTouch: true,
-    touchRatio: 0.5,
+function initializeSwiper() {
+    var screenWidth = window.innerWidth;
+    var slidesPerView;
+
+    if (screenWidth >= 1024) {
+      slidesPerView = 3;
+    } else if (screenWidth >= 768) {
+      slidesPerView = 2;
+    } else {
+      slidesPerView = 1;
+    }
+
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: slidesPerView,
+      spaceBetween: 30,
+      grabCursor: true,
+      simulateTouch: true,
+      touchRatio: 0.5
+    });
+  }
+
+  // Initialize Swiper on page load
+  initializeSwiper();
+
+  // Re-initialize Swiper on window resize
+  window.addEventListener('resize', function() {
+    initializeSwiper();
   });
