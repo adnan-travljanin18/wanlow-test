c12469d2ec2cd421dfc5553089a5904ce96150c5
Adnan Travljanin <adnantravljanin@Adnans-MacBook-Pro.local>
2023-10-17 15:17:12 +0200

    responsivness and adding css to sections for styling

--- assets/instagram-slider.js
+++ assets/instagram-slider.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper(".instagram-slider", {
-      slidesPerView: 4,
-      spaceBetween: 30,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+  var swiper = new Swiper(".instagram-slider", {
+    slidesPerView: 4,
+    spaceBetween: 15,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      992: {
+        slidesPerView: 4,
       },
-    });
+      768: {
+        slidesPerView: 3,  // Show 3 slides per view on tablets (width >= 768px)
+      },
+      0: {
+        slidesPerView: 2,  // Show 2 slides per view on mobile (width >= 576px)
+      }
+    }
   });
-  
+});
--- assets/testimonial-slider.js
+++ assets/testimonial-slider.js
@@ -2,16 +2,13 @@ var swiper = new Swiper(".testimonial-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
+      delay: 4500,
       disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
     },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     loop: true,
+    speed: 1000,
   });
